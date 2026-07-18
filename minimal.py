c22b6a7da5168f32e1f887b267f465b8726a8da2
fengniao-uu <fengniao-uu@users.noreply.github.com>
2026-07-19 01:22:31 +0800

    revert: 恢复正式代码

--- minimal.py
+++
@@ -1,5 +0,0 @@
-from workers import WorkerEntrypoint, Response
-
-class Default(WorkerEntrypoint):
-    async def fetch(self, request):
-        return Response("Hello from Python Worker!", headers={"Content-Type": "text/plain"})
